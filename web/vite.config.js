388498ab5dba19a9d0038372aa91cf9c649e338e
azaZefir <arykov.azat.kg@gmail.com>
2026-02-05 18:11:36 +0600

    dev

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // base: "./" важно, чтобы build работал при открытии dist/index.html как файла
 export default defineConfig({
-  base: "presentation-apt-12",
+  base: "/presentation-apt-12/",
   plugins: [react()],
 });
