ff22f83cc7746d4b59dfa63f7ae4740765e352f8
azaZefir <arykov.azat.kg@gmail.com>
2026-02-05 11:59:54 +0600

    fix

--- web/src/schemes/defaultSchemes.js
+++ web/src/schemes/defaultSchemes.js
@@ -1,15 +1,8 @@
 export const defaultSchemes = {
-  masterUrl: "/schemes/master.svg",
+  master: "", // если надо - тоже url
   blocks: {
-    b01: {
-      defaultUrl: "/schemes/blocks/b1.svg",
-      floors: {
-        1: "/schemes/blocks/b1_floor1.svg",
-      },
-    },
-    b02: {
-      defaultUrl: "/schemes/blocks/b2.svg",
-      floors: { 1: "/schemes/blocks/b2_floor1.svg" },
-    },
+    b01: "/schemes/blocks/b1.svg", // пробел обязательно кодировать
+    b02: "/schemes/blocks/b1.svg",
+    // ...
   },
 };
